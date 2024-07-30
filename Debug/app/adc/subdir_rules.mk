b4302703d75e487b7e36c8ca224db1cf2e5ec14a
Nov4ou <nov4ou@outlook.com>
2024-07-30 14:58:05 +0800

    update

--- Debug/app/adc/subdir_rules.mk
+++ Debug/app/adc/subdir_rules.mk
@@ -6,7 +6,7 @@
 app/adc/adc.obj: ../app/adc/adc.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"/Applications/ti/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla0 --float_support=fpu32 --vcu_support=vcu0 --include_path="/Users/nov4ou/workspace_ccstheia/Double_AC_Voltage_Controllers" --include_path="/Users/nov4ou/workspace_v12/f2806x/headers/include" --include_path="/Users/nov4ou/workspace_v12/f2806x/common/include" --include_path="/Applications/ti/ti-cgt-c2000_22.6.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="app/adc/$(basename $(<F)).d_raw" --obj_directory="app/adc" $(GEN_OPTS__FLAG) "$<"
+	"/Applications/ti/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla0 --float_support=fpu32 --vcu_support=vcu0 --include_path="/Users/nov4ou/workspace_v12/Problems/Double_AC_Voltage_Controllers" --include_path="/Users/nov4ou/workspace_v12/f2806x/headers/include" --include_path="/Users/nov4ou/workspace_v12/f2806x/common/include" --include_path="/Applications/ti/ti-cgt-c2000_22.6.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="app/adc/$(basename $(<F)).d_raw" --obj_directory="app/adc" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
